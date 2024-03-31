7ac73c57b3f9f5cbe77fddb15c977d75746bd260
ShivA <83615352+hubsMIT1@users.noreply.github.com>
2024-03-31 07:38:38 +0530

    Update urls.py

--- notes/urls.py
+++ notes/urls.py
@@ -4,7 +4,7 @@ from .views import NoteListCreateView,NoteListView, NoteRetrieveUpdateDestroyVie
 urlpatterns = [
     path('create', NoteListCreateView.as_view(),name='create-new-note'),
     path('<int:pk>',NoteRetrieveUpdateDestroyView.as_view(),name='rud-on-notes'),
-    # path('',NoteListView.as_view(),name='get-all-notes'),
+    path('',NoteListView.as_view(),name='get-all-notes'),
     path('<int:pk>/share',NoteShareView.as_view(),name='share-notes-with-other-users'),
     path('version-history/<int:id>',NoteVersionHistoryView.as_view(),name='get-notes-history')
 ]
